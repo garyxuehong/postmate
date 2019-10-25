44a2c589bd8d4878beb316b806fac8a5de835635
Gary Xue <Gary.Xue@reece.com.au>
2019-10-25 21:11:54 +1100

    add semantic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { Button } from "semantic-ui-react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button primary>Primary</Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
